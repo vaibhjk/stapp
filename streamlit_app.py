6a8faf489230b491dd4b79cb4e7416df84a807b0
vaibhjk <113019331+vaibhjk@users.noreply.github.com>
2024-05-15 14:11:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
+movies = pickle.load(open('movies.pkl','rb'))
 similarity = pickle.load(open('model/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
